4ff384271a4118663f93f94189f62e073833e69e
solomonakinbiyi <akinbiyisolo22@gmail.com>
2021-10-12 15:02:13 +0100

    a little modification

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ function App() {
     {
       imgUrl:
         "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f4ebe0c87612dab4f12a597%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D292%26cropX2%3D3684%26cropY1%3D592%26cropY2%3D3987",
-      desc: "Bill gates",
-      id: "2Bill gates",
+      desc: "Bill Gates",
+      id: "2Bill Gates",
       comments: [],
       likes: 0,
     },
